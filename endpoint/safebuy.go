4ff88073954a71ae8d9fe12bf32363a8f7e41394
Iago Agualuza <iagoagualuza@id.uff.br>
2020-03-03 17:00:54 -0300

    Save all users logged request to safebuy endpoint in db table risk_analytic

--- endpoint/safebuy.go
+++ endpoint/safebuy.go
@@ -1,6 +1,7 @@
 package endpoint
 
 import (
+	"bank/database"
 	"github.com/labstack/echo"
 	"math"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 )
 
 func SafeBuy(c echo.Context) error {
+
 	Profiles := []Profile{
 		{1, 2000.00},
 		{2, 5000.00},
@@ -28,7 +30,17 @@ func SafeBuy(c echo.Context) error {
 	var response RiskEvaluate
 	response.Profile = p
 	response.Rate = 1 - math.Min(rate,1)
-
+	saveAnalytics(c.FormValue("token"),p,price,rate)
 	return c.JSON(http.StatusOK, response)
 
+}
+
+func saveAnalytics(t string , profile Profile, pp,rt float64){
+	currentUser := getUserByToken(t)
+	db := database.ConnectDB()
+	_ , err := db.Query("INSERT INTO risk_analytic (user_id,payment,product_price,rate) VALUES(?,?,?,?)",currentUser.Id,profile.Payment,pp,rt)
+
+	if(err != nil) {
+		return
+	}
 }
