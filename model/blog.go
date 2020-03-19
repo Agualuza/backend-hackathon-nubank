5cbc418d453b33770e5a4d58270c5c1e03b78015
Agualuza <iagoagualuza@id.uff.br>
2020-03-19 20:13:29 -0300

    Add ReadTime struct and alter Blog struct

--- model/blog.go
+++ model/blog.go
@@ -1,9 +1,15 @@
 package model
 
 type Blog struct {
-	Id     int     `json:"id"`
-	Title  string  `json:"title"`
-	Post   string  `json:"post"`
-	Author   string  `json:"author"`
-	CreatedAt  string  `json:"created_at"`
+	Id        int      `json:"id"`
+	Title     string   `json:"title"`
+	Post      string   `json:"post"`
+	Author    string   `json:"author"`
+	ReadTime  ReadTime `json:"read_time"`
+	CreatedAt string   `json:"created_at"`
+}
+
+type ReadTime struct {
+	Minutes int    `json:"minutes"`
+	Time    string `json:"time"`
 }
